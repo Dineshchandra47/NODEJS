fba32af53a14fd25b46688764998cc022017bc1f
Dineshchandra47 <dineshchandraindia5@gmail.com.com>
2022-12-12 13:27:53 +0530

    changes were made with some files

--- 5-node-boilerplate-public-main/api.js
+++
@@ -1,9 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) =>{ 
-  let users = data;
-  users.map(function(user){
-     let li = document.createElementby ('p')
-  })
-}
-  );
