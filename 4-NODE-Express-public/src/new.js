80b11d7b1bc8f4bba1743143b2f57ffa58bdf6ca
Dineshchandra47 <119965753+Dineshchandra47@users.noreply.github.com>
2022-12-22 08:44:32 +0530

    updated js file

--- 4-NODE-Express-public/src/new.js
+++ 4-NODE-Express-public/src/new.js
@@ -1,14 +1,28 @@
 // console.log('hello puspa');
+
+
 /*  JSON.stringify()*/
+
 let userObj = {
      title: 'JSON.stringify()',
      name: "Sammy",
      email: "sammy@example.com",
      plan: "Pro"
-   };
+  };
    
-   let userStrSpace = JSON.stringify(userObj, null, '---->');
-   console.log(userStrSpace);
+  let userStrSpace = JSON.stringify(userObj, null, '---->');
+  console.log(userStrSpace);
+   
+   
+OUTPUT:
+   
+  {
+---->"title": "JSON.stringify()",
+---->"name": "Sammy",
+---->"email": "sammy@example.com",
+---->"plan": "Pro"
+}
+
 
 // -------------------------------------------------------------------------------------------------
 
@@ -23,3 +37,12 @@ let userObj1 = JSON.parse(userStr, (key, value) => {
   return value;
 });
 console.log(userObj1,);
+
+OUTPUT:
+
+{
+  title: 'JSONPARSE',
+  name: 'SAMMY',
+  email: 'SAMMY@EXAMPLE.COM',
+  plan: 'PRO'
+}
